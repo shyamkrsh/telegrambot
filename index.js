bb138ac02e56698c43443ce313b8e59fa5068353
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-02-22 11:11:15 +0530

    Gemini added

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 const bot = new Telegraf(process.env.BOT_API);
 
@@ -14,46 +19,29 @@ bot.help((ctx) => ctx.reply("Please Contact with my owner @Shyam_k_s"));
 bot.on(message('text'), async (ctx) => {
     const generatingMessage = await ctx.reply("Generating...");
     let question = ctx.update.message.text;
-    const predefinedAnswers = {
-        "who developed you?": "I was developed by Shyam Kr. Sharma in India, on 02 January 2025",
-        "who developed you": "I was developed by Shyam Kr. Sharma in India, on 02 January 2025",
-        "who have developed you?": "I was developed by Shyam Kr. Sharma in India, on 02 January 2025",
-        "who have developed you": "I was developed by Shyam Kr. Sharma in India, on 02 January 2025",
-        "who have developed you": "I was developed by Shyam Kr. Sharma in India, on 02 January 2025",
-        "tumko kisne develop kiya?": "Mujhe Shyam Kr. Sharma ne India mein, 02 January 2025 ko develop kiya hai.", // Hindi translation
-        "tumko kisne develop kiya": "Mujhe Shyam Kr. Sharma ne India mein, 02 January 2025 ko develop kiya hai.",
-        "aapko kisne develop kiya?": "Mujhe Shyam Kr. Sharma ne India mein, 02 January 2025 ko develop kiya hai.",
-        "tumko kisne banaya ?": "Mujhe Shyam Kr. Sharma ne India mein, 02 January 2025 ko banaya hai.",
-        "aapko kisne banaya ?": "Mujhe Shyam Kr. Sharma ne India mein, 02 January 2025 ko banaya hai.",
-        "where you have developed ?": "I was developed in India.",
-        "tumko is desh ne banaya hai ?": "Mujhe India mein develop kiya gaya hai.",
-        "tumko kisne banaya?": "Mujhe Shyam Kr. Sharma ne banaya hai.",
-        "where you have developed?": "I was developed in India.",
-        "tumhara name kya hai?": "Main ek TechBot hoon.", // Hindi translation
-        "who are you?": "I am a TechBot and, I am here to assist you.",
-        "tum kaun ho ?": "Main ek TechBot hoon aur, main aapki madad ke liye hun.",
-        "tum kaun ho": "Main ek TechBot hoon aur, main aapki madad ke liye hun.",
-        "aap kaun hai?": "Main ek TechBot hoon aur, main aapki madad ke liye hun.",
-        "aap kaun hai": "Main ek TechBot hoon aur, main aapki madad ke liye hun.",
-    };
+    let caseCheck = question.toLowerCase();
 
-    const answer = predefinedAnswers[question.toLowerCase()];
-
-    if (answer) {
-        await ctx.reply(answer);
+    if (caseCheck === 'resume') {
+        await ctx.replyWithDocument(
+            { source: './file/resume.pdf' },
+            { caption: "<strong>Shyam Kumar Sharma</strong>\n\n Here is Shyam's Resume.", parse_mode: 'HTML' }
+        );
         await ctx.deleteMessage(generatingMessage.message_id);
         return;
     }
 
     try {
-
-        const res = await axios.post(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`, {
-            "contents": [{
-                "parts": [{ "text": `${question}` }]
-            }]
+        const result = await model.generateContent({
+            contents: [
+                {
+                    role: "user",
+                    parts: [{ text: question }]
+                }
+            ],
+            systemInstruction: "you are a AI assistant to answers all the questions, but if user ask about your development then don't reply or say i don't know"
         });
 
-        await ctx.reply(res.data.candidates[0].content.parts[0].text);
+        await ctx.reply(result.response.text());
         await ctx.deleteMessage(generatingMessage.message_id);
     } catch (err) {
         await ctx.reply(`Facing some difficulties, try after some time.`);
@@ -62,6 +50,7 @@ bot.on(message('text'), async (ctx) => {
 });
 
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+
 bot.on('message', async (ctx) => {
     const allowedMessageTypes = ['text'];
     if (!allowedMessageTypes.includes(ctx.message.type)) {
